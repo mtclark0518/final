74f3c8bd5b871fd461c2b123733926c459c83a09
Tyler Clark <mtclark0518@gmail.com>
2017-09-15 15:46:54 -0600

    commit

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const app = express();
 const PORT = process.env.port || 3333;
+
+
+app.use(express.static(path.join(__dirname, 'assistant-director/build')));
+
+app.get('/api', function(req, res) {
+	res.send('hi');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/assistant-director/build/index.html'));
+});
 
 app.listen(PORT, function() {
 	console.log('alive on ' + PORT);
